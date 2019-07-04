27bdd08a49f0c6574da1bcb01f189545d69f2df9
Clément Janssens <clement.janssens.ti@gmail.com>
2019-07-04 15:56:10 +0200

    Animations + date & time picker added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
 import './Header.css'
+import './Animations.css'
 import Logo from '../images/logo.png'
 
 class Header extends Component {
     render() {
+        console.log(this.props.withBackButton)
+
         return (
             <header className='shadow'>
                 <div className='content'>
-                    {this.props.withBackButton ? <Link to='/'><div className='header-back-btn' onClick={this.handleGoToBack}><span className="icon-arrow-left"></span></div></Link> : null}
+                    <CSSTransition in={this.props.withBackButton} mountOnEnter unmountOnExit timeout={100} classNames='slide'>
+                        <Link to='/'><div className='header-back-btn' onClick={this.handleGoToBack}><span className="icon-arrow-left"></span></div></Link>
+                    </CSSTransition>
 
                     <div className='logo' onClick={this.handleGoBack}>
                         <img src={Logo} alt='Reliability Score logo' />
                         <h1>Reliability Score</h1>
                     </div>
                 </div>
-            </header>
+            </header >
         )
     }
 }
