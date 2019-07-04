ffb605f76d4499bb6f29c9b66f07b013960511cb
Clément Janssens <clement.janssens.ti@gmail.com>
2019-07-04 10:09:24 +0200

    Remove unused variables

--- src/components/DirectionItem.js
+++ src/components/DirectionItem.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import propTypes from 'prop-types'
 import { Timeline } from 'antd'
 import OrangeWarningIcon from '../images/orange-warning.png'
 import WhiteWarningIcon from '../images/white-warning.png'
